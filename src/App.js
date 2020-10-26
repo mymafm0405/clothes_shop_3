ad5d4e70cfe852a44749b011a10010ef347b3d5e
Mahmoud <mymafm0405@gmail.com>
2020-10-26 20:01:48 +0300

    after using redux but sign out not working as expected

--- src/App.js
+++ src/App.js
@@ -8,24 +8,21 @@ import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header/header.component';
 import { auth, createUserProfile } from './firebase/firebase.utiles';
+import {connect} from 'react-redux';
+import {setCurrentUser} from './redux/user/user.actions'
 // import TestComp from './test'
 
 class App extends React.Component {
-  constructor() {
-    super();
-    
-    this.state = {
-      currentUser: null
-    }
-  }
   unSubscribeFromAuth = null;
 
   componentDidMount() {
+    const {setCurrentUser} = this.props;
+    
     this.unSubscribeFromAuth = auth.onAuthStateChanged( async authUser => {
       if (authUser) {
         const userRef = await createUserProfile(authUser)
         userRef.onSnapshot(snapShot => {
-          this.setState({
+          setCurrentUser({
             currentUser: {
               id: snapShot.id,
               ...snapShot.data()
@@ -33,7 +30,7 @@ class App extends React.Component {
           }, () => {
             console.log(this.state.currentUser)
           })
-          this.setState({currentUser: authUser})
+          setCurrentUser(authUser)
         })
       } else {
         this.setState({
@@ -51,7 +48,7 @@ class App extends React.Component {
     return (
       <div>
         {/* <TestComp/> */}
-        <Header currentUser={this.state.currentUser}/>
+        <Header/>
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
@@ -61,5 +58,8 @@ class App extends React.Component {
     );
   }
 }
+const mapDispatchToProps = dispatch => ({
+  setCurrentUser: user => dispatch(setCurrentUser(user))
+})
 
-export default App;
+export default connect(null, mapDispatchToProps) (App);
