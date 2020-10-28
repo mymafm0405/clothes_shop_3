b8bf1b5159d53953bc3d6d96ebcbecd29f87fd58
Mahmoud <mymafm0405@gmail.com>
2020-10-28 19:02:29 +0300

    After adding the quantity property to the item when adding it

--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -3,3 +3,8 @@ import {CartTypes} from './cart.types'
 export const toggleCartDropdown = () => ({
     type: CartTypes.TOGGLE_CART_DROPDOWN
 })
+
+export const addItem = item => ({
+    type: CartTypes.ADD_ITEM,
+    payload: item
+})
