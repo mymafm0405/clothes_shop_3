782f5a46c47d04a5651391ff3a472faf81eb0a40
Mahmoud <mymafm0405@gmail.com>
2020-11-03 20:09:12 +0300

    After finish cart item and checkout page

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import {selectCurrentUser} from './redux/user/user.selectors'
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import Checkout from './pages/checkout/checkout.component'
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header/header.component';
 import { auth, createUserProfile } from './firebase/firebase.utiles';
@@ -52,6 +53,7 @@ class App extends React.Component {
         <Header/>
         <Switch>
           <Route exact path='/' component={HomePage} />
+          <Route exact path='/checkout' component={Checkout} />
           <Route path='/shop' component={ShopPage} />
           <Route exact path='/signin' render={() => 
             this.props.currentUser ? (<Redirect to='/'/>) : (<SignInAndSignUpPage/>)
